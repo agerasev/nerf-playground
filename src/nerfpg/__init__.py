f203202f9a34ea4a70f9d45d2cb52c9e9bb07da9
Alexey Gerasev <alexey.gerasev@gmail.com>
2024-12-23 08:43:11 +0300

    Raymarching

---
+++ src/nerfpg/__init__.py
@@ -0,0 +1,42 @@
+from PIL import Image
+import torch as tch
+import numpy as np
+
+
+def image_to_tensor(img: Image, dev: tch.device) -> tch.Tensor:
+    return (tch.tensor(np.array(img)).to(dev).type(tch.float32) / 255.0).permute(
+        (2, 0, 1)
+    )
+
+
+def tensor_to_image(t: tch.Tensor) -> Image:
+    s = (t.clip(0.0, 1.0) * 255.0).type(tch.uint8).permute((1, 2, 0))
+    return Image.fromarray(s.numpy(force=True))
+
+
+def grid(shape: tuple[int, int], dev: tch.device) -> tch.Tensor:
+    """
+    Rectangular grid of coordinates.
+    Returned tensor shape: (shape[0], shape[1], 2)
+    """
+    x = tch.arange(shape[0]) / (shape[0] - 1)
+    x = tch.stack((x, tch.ones_like(x)), dim=-1)
+    x = x.unsqueeze(1).to(dev)
+
+    y = tch.arange(shape[1]) / (shape[1] - 1)
+    y = tch.stack((tch.ones_like(y), y), dim=-1)
+    y = y.unsqueeze(0).to(dev)
+
+    return x * y
+
+
+def encode_coord(coord: tch.Tensor, n_modes: int) -> tch.Tensor:
+    """
+    Cosine coordinate encoding.
+    + `coord` shape: (N, C)
+    + Returned tensor shape: (N, C * n_modes)
+    """
+    freqs = tch.arange(1, n_modes + 1, device=coord.device) * np.pi
+
+    args = coord.unsqueeze(-1) * freqs.reshape((1, 1, -1))
+    return args.cos().flatten(1, 2)
